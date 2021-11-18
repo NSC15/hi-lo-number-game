9f946126fd8693928bb8d848d3c571e600f2f836
NSC15 <nathancave123@gmail.com>
2021-11-18 18:12:30 +0000

    add JS function to get buttons from HTML once the full page has loaded, add functionality to difficulty buttons which will then run the appropriate game based off the user button click. tested via console.log messages and validated process is working through chrome dev tools

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,25 @@
+document.addEventListener("DOMContentLoaded", function () {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function () {
+            if (this.getAttribute("data-type") === "easy") {
+                runGame1()
+                console.log("running game 1")
+            }
+            if (this.getAttribute("data-type") === "medium") {
+                runGame2()
+                console.log("running game 2")
+            }
+            if (this.getAttribute("data-type") === "hard") {
+                runGame3()
+                console.log("running game 3")
+            }
+
+        });
+    }
+})
+
 function runGame1() {
 
 }
