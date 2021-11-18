033d0d0184ce31169549905805d0f4e3ac75101e
NSC15 <nathancave123@gmail.com>
2021-11-18 17:31:45 +0000

    add id's to spans for bank, lives and rnd number. add basic JS function plan

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,31 @@
-console.log("test");
+function runGame1() {
+
+}
+
+function runGame2() {
+
+}
+
+function runGame3() {
+
+}
+
+function calculateCorrectAnswer() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function addBank() {
+
+}
+
+function takeLife() {
+
+}
+
+function highScore() {
+
+}
