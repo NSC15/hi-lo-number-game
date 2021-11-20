830b87027f09890c7099c58b64b10d1cad579cab
NSC15 <nathancave123@gmail.com>
2021-11-20 10:00:00 +0000

    add a submit answer button to HTML, re write starting JS to identify the difficulty variable and then create random numbers for each difficulty easy is 0-10 medium is 0-15 and hard is 0-20. in the run game function code is now in place so when the rungame function starts the if statment then identifies which game to play dependant on the user input

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,42 +1,47 @@
+/*wait till the full page is loaded before running the game.*/
 document.addEventListener("DOMContentLoaded", function () {
     let buttons = document.getElementsByTagName("button");
 
     for (let button of buttons) {
         button.addEventListener("click", function () {
-            if (this.getAttribute("data-type") === "easy") {
-                runGame1()
-                console.log("running game 1")
+            if (this.getAttribute("data-type") === "submit") {
+                checkInput();
+            } else {
+                let difficulty = this.getAttribute("data-type");
+                runGame(difficulty);
             }
-            if (this.getAttribute("data-type") === "medium") {
-                runGame2()
-                console.log("running game 2")
-            }
-            if (this.getAttribute("data-type") === "hard") {
-                runGame3()
-                console.log("running game 3")
-            }
-
         });
     }
 })
 
-function runGame1() {
 
+
+function runGame(difficulty) {
+    /*create random numbers for each game difficulty, run game difficulty based on user click*/
+    let easyNum = Math.floor(Math.random() * 10);
+    let medNum = Math.floor(Math.random() * 15);
+    let hardNum = Math.floor(Math.random() * 20);
+
+    if (diffculty === "easy") {
+        runGame1()
+    } else if (difficulty === "medium") {
+        runGame2()
+    } else if (difficulty === "hard") {
+        runGame3()
+    }
 }
 
-function runGame2() {
+function runGame1(easyNum) {};
 
-}
+function runGame2(medNum) {};
 
-function runGame3() {
-
-}
+function runGame3(hardNum) {};
 
 function calculateCorrectAnswer() {
 
 }
 
-function checkAnswer() {
+function checkInput() {
 
 }
 
