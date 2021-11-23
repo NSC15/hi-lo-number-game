12af7e1201e0319b3b1e19eeb0bebcc8da6e68d5
NSC15 <nathancave123@gmail.com>
2021-11-23 19:38:13 +0000

    re assess JS code layout, remove submit button, simplfy code into small singular functions for when needing to work out correct answer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,50 +1,36 @@
-/*wait till the full page is loaded before running the game.*/
+/*Wait till the full page is loaded before running the game.*/
 document.addEventListener("DOMContentLoaded", function () {
     let buttons = document.getElementsByTagName("button");
 
     for (let button of buttons) {
         button.addEventListener("click", function () {
-            if (this.getAttribute("data-type") === "submit") {
+            if (this.getAttribute("data-type") === "higher") {
+                checkInput();
+            } else if (this.getAttribute("data-type") === "lower") {
                 checkInput();
             } else {
                 let difficulty = this.getAttribute("data-type");
                 runGame(difficulty);
+
             }
         });
     }
 })
 
+/*Generate Random Numbers for each difficulty*/
+function randomNum(userNum) {
+    userNum = Math.ceil(Math.random() * 10);
+    return userNum
 
-
-function runGame(difficulty) {
-    /*create random numbers for each game difficulty, run game difficulty based on user click*/
-    let easyNum = Math.floor(Math.random() * 10);
-    let medNum = Math.floor(Math.random() * 15);
-    let hardNum = Math.floor(Math.random() * 20);
-
-    if (difficulty === "easy") {
-        runGame1(easyNum)
-    } else if (difficulty === "medium") {
-        runGame2(medNum)
-    } else if (difficulty === "hard") {
-        runGame3(hardNum)
-    }
 }
-/* maniupulates the dom by displaying the generated random number from the rungame function into the allocated span in the html*/
-function runGame1(easyNum) {
-    document.getElementById("game-num").textContent = `${easyNum}`;
-};
+/*main rungame function declaring shown number and number for checking higher or lower*/
+function runGame(difficulty) {
+    let userNum = randomNum(difficulty);
+    displayNum(userNum);
+}
 
-function runGame2(medNum) {
-    document.getElementById("game-num").textContent = `${medNum}`;
-};
-
-function runGame3(hardNum) {
-    document.getElementById("game-num").textContent = `${hardNum}`;
-};
-
-function calculateCorrectAnswer() {
-
+function displayNum(userNum) {
+    document.getElementById("game-num").textContent = `${userNum}`
 }
 
 function checkInput() {
